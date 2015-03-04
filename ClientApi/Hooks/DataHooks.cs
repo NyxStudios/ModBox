9a6cf75d7f070b5728da7afe7d5e6c5d7e03e37e
Zack Piispanen <olink@shankshock.com>
2015-03-04 01:11:22 -0500

    Commit missing files, wtf windows.

---
+++ ClientApi/Hooks/DataHooks.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using ClientApi.Networking;
+
+namespace ClientApi.Hooks
+{
+	public class GetDataEventArgs : HandledEventArgs
+	{
+		public byte Packet { get; private set; }
+		public byte[] Buffer { get; private set; }
+
+		public GetDataEventArgs(byte packet, byte[] buffer)
+		{
+			Packet = packet;
+			Buffer = buffer;
+		}
+	}
+
+	public class SendDataEventArgs : HandledEventArgs
+	{
+		public byte Packet { get; private set; }
+		public byte[] Buffer { get; private set; }
+
+		public SendDataEventArgs(byte packet, byte[] buffer)
+		{
+			Packet = packet;
+			Buffer = buffer;
+		}
+	}
+
+	public class GetCustomPacketEventArgs : HandledEventArgs
+	{
+		public IPacket Packet { get; set; }
+
+		public GetCustomPacketEventArgs(IPacket packet)
+		{
+			Packet = packet;
+		}
+	}
+
+	public class DataHooks
+	{
+		public delegate void GetDataD(GetDataEventArgs args);
+		public static event GetDataD GetData;
+
+		public delegate void SendDataD(SendDataEventArgs args);
+		public static event SendDataD SendData;
+
+		public delegate void GetCustomPacketD(GetCustomPacketEventArgs args);
+		public static event GetCustomPacketD GetCustomPacket;
+
+		public static bool OnGetData(GetDataEventArgs args)
+		{
+			if (GetData == null)
+			{
+				return false;
+			}
+
+			GetData(args);
+			return args.Handled;
+		}
+
+		public static bool OnSendData(SendDataEventArgs args)
+		{
+			if (SendData == null)
+			{
+				return false;
+			}
+
+			SendData(args);
+			return args.Handled;
+		}
+
+		public static bool OnGetCustomPacket(IPacket packet)
+		{
+			if (GetCustomPacket == null)
+			{
+				return false;
+			}
+
+			GetCustomPacketEventArgs args = new GetCustomPacketEventArgs(packet);
+			GetCustomPacket(args);
+			return args.Handled;
+		}
+	}
+}
---
+++ ClientApi/Networking/Packets/NpcLimit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ClientApi.Utils;
+
+namespace ClientApi.Networking.Packets
+{
+	[PacketId(PacketId.NpcLimit)]
+	public class NpcLimit : IPacket
+	{
+		public int Limit { get; set; }
+
+		public NpcLimit(int limit)
+		{
+			Limit = limit;
+		}
+
+		public void Write(Stream s)
+		{
+			s.WriteInt32(Limit);
+		}
+
+		public void Read(Stream s)
+		{
+			Limit = s.ReadInt32();
+		}
+	}
+}
